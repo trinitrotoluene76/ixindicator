099012ded16cad6ab464b30b6ba1da5e35953655
amaury.froment <amaury.froment@gmail.com>
2024-04-27 22:38:37 +0200

    iXindicator.ino add display time
    ajouts var posY, chrono
    ca compile mais chrono figé à 0 au $START

--- iXindicator.ino
+++ iXindicator.ino
@@ -1,6 +1,9 @@
 //iXindicator fonctionnant sur Lyligo T-Dongle-S3
 //indicateur d'état commandé par l'envoi de commande via USB
 
+// convertisseur en ligne: https://www.convert-me.com/fr/convert/time/millisecond/millisecond-to-dhms.html?u=millisecond&v=9.999.354.000
+// le nombre de jour max affichable est: 4,294,967,295ms (2^32 - 1) soit 49 jours 17:02
+
 #include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
 #include <SPI.h>
 
@@ -16,10 +19,24 @@ TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 const int progressBarHeight = 4; //TODO
 const int progressBarMargin = 15; //TODO
 const int duration = 5000; //TODO variable de test à supprimer
+const int posY_L1=10;
+const int posY_L2=30;
+const int posY_L3=50;
+
 int screenHeight = 0; //défini dans setup
 int screenWidth = 0; //défini dans setup
 
+// USB
 USBCDC USBSerial;
+
+// Chrono
+unsigned long startTime = 0;
+unsigned long elapsedTime = 0;
+bool maxdays =false; //variable passe à true si le chrono dépasse 49 jours 
+unsigned long currentMillis =0;
+unsigned long maxMillis =4294967295-1000; // marge de sécurité d'1s (49j- 1s)
+unsigned long previousMillis = 0;
+const unsigned long interval = 1000; // Interval d'actualisation d'une seconde
 
 void setup() {
   // Définition de la liaison série
@@ -49,10 +66,73 @@ void setup() {
   tft.setCursor(5, 45);
   tft.println("command");
   tft.setTextSize(2);
-}
+}//fin setup
 
-unsigned long startTime = 0; // Variable pour enregistrer le temps de début
-unsigned long elapsedTime = 0;
+// fonction d'affichage du temps sous différents format
+// hh:mm:ss
+// dd jours hh:mm
+// > 49 jours
+void displayTime(unsigned long elapsed, unsigned long days, bool flag = false) {
+  unsigned long seconds = elapsed;
+  unsigned long minutes = seconds / 60;
+  unsigned long hours = minutes / 60;
+  int textSize=0;
+  int posX =0;
+  if (flag==false){
+    if (days == 0) {
+      // Affiche le temps au format "hh:mm:ss"
+      unsigned long secondsRemainder = seconds % 60;
+      unsigned long minutesRemainder = minutes % 60;
+      unsigned long hoursRemainder = hours % 24;
+      tft.fillScreen(TFT_BLACK); // Efface l'écran
+      tft.setCursor(0, 0);
+
+      // Taille du texte
+      textSize = tft.textWidth(String("00:00:00"));
+      
+      // Calcul de la position x pour centrer le texte
+      posX = (screenWidth - textSize) / 2;
+      tft.setCursor(posX, posY_L2); // Position y au milieu de l'écran
+
+      // Afficher le texte centré
+      tft.printf("%02lu:%02lu:%02lu", hoursRemainder, minutesRemainder, secondsRemainder);
+
+    } else {
+      // Affiche le temps au format "jj jours hh:mm"
+      unsigned long hoursRemainder = hours % 24;
+      unsigned long minutesRemainder = minutes % 60;
+      
+      // Taille du texte
+      textSize = tft.textWidth(String("00 jours"));
+
+      // Calcul de la position x pour centrer le texte
+      posX = (screenWidth - textSize) / 2;
+      // Calcul de la position y pour centrer le texte
+      tft.setCursor(posX, posY_L1);
+
+      tft.fillScreen(TFT_BLACK); // Efface l'écran
+      tft.printf("%03lu jours", days);
+
+      // Taille du texte
+      textSize = tft.textWidth(String("00:00"));
+      // Calcul de la position x pour centrer le texte
+      posX = (screenWidth - textSize) / 2;
+      tft.setCursor(posX, posY_L2);
+      tft.printf("%02lu:%02lu", hoursRemainder, minutesRemainder);
+    }
+  }
+  else{
+    tft.fillScreen(TFT_BLACK);
+    //texte:
+    String message =String("> 49 jours");
+    // Taille du texte
+    textSize = tft.textWidth(message);
+    // Calcul de la position x pour centrer le texte
+    posX = (screenWidth - textSize) / 2;
+    tft.setCursor(posX, posY_L1);
+    tft.println(message);
+  }
+}//fin diplayTime
 
 void loop() {
   // Lecture des données sur la liaison série
@@ -64,39 +144,38 @@ void loop() {
     // USBSerial.println(receivedData); // Affiche les données reçues
 
     if (strcmp(receivedData, "$START") == 0){
-    // Démarre le chronomètre lorsque des données sont disponibles
-    startTime = millis();
-  
+      // Démarre le chronomètre lorsque des données sont disponibles
+      startTime = millis(); //démarre le chrono
+      //si on n'a pas atteint le nb de jour max affichable on affiche le compteur, sinon on sature à "> 49 jours"
+      if(maxdays==false && millis()< maxMillis){
+      currentMillis = millis();
+      }
+      else{
+        currentMillis=maxMillis;
+        maxdays=true;
+      }
+      // on rafraichit l'écran que toutes les secondes (=interval)
+      if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        
+        unsigned long secondsElapsed = (currentMillis - startTime) / 1000;
+        unsigned long days = secondsElapsed / 86400; // Nombre de jours écoulés
+        unsigned long secondsRemainder = secondsElapsed % 86400; // Secondes restantes après le dernier jour complet
+        elapsedTime = secondsRemainder; // Met à jour le temps écoulé en prenant en compte les secondes restantes après le dernier jour complet
+        // si on s'approche de 2^32-1 (i.e la limite encodable alors on sature l'affichage)
+        if (maxdays==false) {
+          displayTime(elapsedTime, days);
+        }else{
+          displayTime(0, 49, true);
+          }//fin du else
+      }//fin du if
     // displayPassed();
     // delay(1000);
     // displayFailed();
     // delay(1000);
-    }
-    displayChrono();
-    delay(1000);
-  }
-}
-
-void displayChrono() {
- elapsedTime = millis() - startTime;
-
-  tft.setTextSize(2);
-  tft.setTextColor(TFT_BLACK); // Couleur du texte
-  
-  //texte:
-  String message =String(elapsedTime / 1000) + " s";
-  // Taille du texte
-  int textSize = tft.textWidth(message);
-
-  // Calcul de la position x pour centrer le texte
-  int posX = (screenWidth - textSize) / 2;
-  // Calcul de la position y pour centrer le texte
-  int posY = 54;
-  tft.setCursor(posX, posY); // Position y au milieu de l'écran
-
-  // Afficher le texte centré
-  tft.println(message);  
-}
+    }//fin du if $START
+  }//fin du if USBSerial
+}//fin du loop
 
 void displayPassed() {
   tft.setTextColor(TFT_BLACK); // Couleur du texte
@@ -105,14 +184,10 @@ void displayPassed() {
   String message = "PASSED";
   // Taille du texte
   int textSize = tft.textWidth(message);
-  int textHeight = tft.fontHeight();
-  // Calcul de la position x pour centrer le texte
   int posX = (screenWidth - textSize) / 2;
-  // Calcul de la position y pour centrer le texte
-  int posY = (screenHeight - textHeight) / 2;
-  tft.setCursor(posX, posY); // Position y au milieu de l'écran
+
   tft.fillScreen(tft.color565(0, 255, 0)); // écran vert
-  tft.setCursor(posX, posY); // Position y au milieu de l'écran
+  tft.setCursor(posX, posY_L3); // Position y au milieu de l'écran
   // Afficher le texte centré
   tft.println(message);  
 }
